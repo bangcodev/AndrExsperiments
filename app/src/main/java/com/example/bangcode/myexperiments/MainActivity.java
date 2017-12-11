e786a0d1bad532e73056d1f3942bcd9abe911d98
wahyu firmansyah <wahyu.firmansyah082@gmail.com>
2017-12-12 05:44:36 +0700

    [Done] - Belajar Log D - Belajar Manifest - Belajar Build Gradle

--- app/src/main/java/com/example/bangcode/myexperiments/MainActivity.java
+++ app/src/main/java/com/example/bangcode/myexperiments/MainActivity.java
@@ -2,12 +2,20 @@ package com.example.bangcode.myexperiments;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // TODO: 12/12/17 Belajar Log D 
+
+        Log.d(LOG_TAG,"Hello World");
+
     }
 }
